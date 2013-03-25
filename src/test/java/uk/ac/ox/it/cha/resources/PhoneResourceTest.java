a2db6c59d4480596dcf745da64feeda897fc3b42
Martin Filliau <martin@filliau.com>
2013-03-25 16:42:41 +0000

    Empty test resource (placeholder for testing resource Phone).

---
+++ src/test/java/uk/ac/ox/it/cha/resources/PhoneResourceTest.java
@@ -0,0 +1,18 @@
+package uk.ac.ox.it.cha.resources;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author martinfilliau
+ */
+public class PhoneResourceTest {
+    
+    public PhoneResourceTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
